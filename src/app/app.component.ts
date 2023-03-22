6e81dbb865a5c539c8c7230b54dafa9d765443c9
Mobeena Afzal <htmlcssjavascriptangular@gmail.com>
2023-03-22 10:41:09 +0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  tasks :any = [];
   title = 'notpad1';
+ 
+  newtask(data: string){
+    if(data != ""){
+      this.tasks.push({id:this.tasks.length, title: data})
+      console.log(this.tasks)
+    }
+     
+  }
+  removetask(id:number){
+    this.tasks = this.tasks.filter((item: {id : number}) => item.id != id)
+  }
+  removeall(){
+    this.tasks = [];
+  }
+ 
 }
